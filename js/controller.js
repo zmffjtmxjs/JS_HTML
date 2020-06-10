8a0f326be4c970e8da06dea5b55af2f2bd5b4d6c
Clustern <homerun372@gmail.com>
2020-06-11 03:00:58 +0900

    더미 데이터 추가 코드 분리, 유효성 검사 코드 추가, 불필요 코드 제거.

--- js/controller.js
+++ js/controller.js
@@ -1,76 +1,117 @@
-var member_index = 1;
+var G_name = document.getElementById("G_name").value;
+var G_age = document.getElementById("G_age").value;
+var G_phone_number = document.getElementById("G_phone_number").value;
+var G_join_date = document.getElementById("G_join_date").value;
+var G_payment_date = document.getElementById("G_payment_date").value;
+var G_endup_date = document.getElementById("G_endup_date").value;
+var G_weight_at_join = document.getElementById("G_weight_at_join").value;
+var G_weight_at_update = document.getElementById("G_weight_at_update").value;
+var G_target_weight = document.getElementById("G_target_weight").value;
+
+//날짜 유효성 검사
+function isNotDatetime(d){
+    var re = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+    //         yyyy -       MM      -       dd           
+    if (re.test(d))
+        return false;
+    return true;
+}
+
+//전화번호 유효성 검사
+function isNotCellPhone(str) {
+	var regTel = /^(01[016789]{1}|070|02|0[3-9]{1}[0-9]{1})-[0-9]{3,4}-[0-9]{4}$/;
+	if(!regTel.test(str)) {
+		return true;
+	}
+	return false;
+};
+
+//종합적인 입력값 유효성 검사
+function G_data_checker(){
+    {/* 출력 값 테스트
+    console.log("이름 빔 :                 "+(G_name === ''));
+    console.log("나이 빔 :                 "+(G_age == ''));
+    console.log("나이 포맷 불효 :          "+(Number.isInteger(G_age)));
+    console.log("전화번호 빔 :             " + (G_phone_number == ''));
+    console.log("전화번호 포맷 불효 :      "+ (isNotCellPhone(G_phone_number)));
+    console.log("가입일 빔 :               "+(G_join_date == ''));
+    console.log("가입일 포맷 불효 :        " + (isNotDatetime(G_join_date)));
+    console.log("만료일 빔 :               "+(G_endup_date == ''));
+    console.log("만료일 포맷 불효 :        "+(isNotDatetime(G_endup_date)));
+    console.log("가입시 몸무게 빔 :        "+(G_weight_at_join == ''));
+    console.log("가입시 몸무게 포맷 불효 : "+(Number.isInteger(G_weight_at_join)));
+    console.log("목표 몸무게 빔 :          "+(G_target_weight == ''));
+    console.log("목표 몸무게 포맷 불효 :   "+(Number.isInteger(G_target_weight)));
+    console.log("성별 선택 안함 :         "+(document.getElementById("male").checked === false)&&(document.getElementById("female").checked === false));
+    */}
+    if(G_name === ''){
+        alert("이름을 입력해주세요.");
+        return false;
+    } else if((G_age == '') || (Number.isInteger(G_age))){
+        alert("입력한 나이를 확인해주세요.");
+        return false;
+    } else if((G_phone_number == '') || (isNotCellPhone(G_phone_number))){
+        alert("입력한 전화번호를 확인해주세요.");
+        return false;
+    } else if((G_join_date == '') || (isNotDatetime(G_join_date))){
+        alert("입력한 가입일을 확인해주세요.");
+        return false;
+    } else if((G_endup_date == '') || (isNotDatetime(G_endup_date))){
+        alert("입력한 만료일을 확인해주세요.");
+        return false;
+    } else if((G_weight_at_join == '') || (Number.isInteger(G_weight_at_join))){
+        alert("입력한 현재 몸무게를 확인해주세요.");
+        return false;
+    } else if((G_target_weight == '') || (Number.isInteger(G_target_weight))){
+        alert("입력한 목표 몸무게를 확인해주세요.");
+        return false;
+    } else if((document.getElementById("male").checked === false)&&(document.getElementById("female").checked === false)){
+        alert("성별을 체크해주세요.");
+        return false;
+    } else{
+        return true;
+    }
+}
 
 function member_add(){
-    var G_name = document.getElementById("G_name").value;
-    var G_age = document.getElementById("age").value;
-    var G_phone_number = document.getElementById("phone_number").value;
-    var G_join_date = document.getElementById("join_date").value;
-    var G_payment_date = document.getElementById("payment_date").value;
-    var G_endup_date = document.getElementById("endup_date").value;
-    var G_weight_at_join = document.getElementById("weight_at_join").value;
-    var G_weight_at_update = document.getElementById("weight_at_update").value;
-    var G_target_weight = document.getElementById("target_weight").value;
-    //테이블 행 추가를 위한 변수
-    if((document.getElementById("male").checked === true)||(document.getElementById("female").checked === true)){
-        var table = document.getElementById("membertable");
-        var row = table.insertRow(-1);      //마지막 부분에 행 추가
+    G_name = document.getElementById("G_name").value;
+    G_age = document.getElementById("G_age").value;
+    G_phone_number = document.getElementById("G_phone_number").value;
+    G_join_date = document.getElementById("G_join_date").value;
+    G_endup_date = document.getElementById("G_endup_date").value;
+    G_weight_at_join = document.getElementById("G_weight_at_join").value;
+    G_target_weight = document.getElementById("G_target_weight").value;
+    var checker = G_data_checker();
 
-        var col = 0;
-        var cell = new Array()
-        while(col < 11) {
-            cell[col] = row.insertCell(col)
-            col = col + 1;
-        }
-
-        cell[0].innerHTML = member_index+` <input type="radio" name="slect" value="`+ member_index +`"></input>`;
-        cell[1].innerHTML = G_name;
-        cell[2].innerHTML = G_age;
-        if(document.getElementById("male").checked === true) {
-            cell[3].innerHTML = "남성";
-        } else if(document.getElementById("female").checked === true) {
-            cell[3].innerHTML = "여성";
-        } 
-        cell[4].innerHTML = G_phone_number;
-        cell[5].innerHTML = G_join_date;
-        cell[6].innerHTML = G_payment_date;
-        cell[7].innerHTML = G_endup_date;
-        cell[8].innerHTML = G_weight_at_join;
-        cell[9].innerHTML = G_weight_at_update;
-        cell[10].innerHTML = G_target_weight;
-
-        member_index = member_index + 1;
-    }else {
-        alert("성별을 선택해주십시오.");
-        return;
+    if(checker){
+            //테이블 행 추가를 위한 변수
+            var table = document.getElementById("membertable");
+            var row = table.insertRow(-1);      //마지막 부분에 행 추가
+            //열 추가
+            var col = 0;                        
+            var cell = new Array()
+            while(col < 11) {
+                cell[col] = row.insertCell(col)
+                col = col + 1;
+            }
+            // 열 내용 추가
+            cell[0].innerHTML = member_index+` <input type="radio" name="slect" value="`+ member_index +`"></input>`;
+            cell[1].innerHTML = G_name;
+            cell[2].innerHTML = G_age;
+            if(document.getElementById("male").checked === true) {
+                cell[3].innerHTML = "남성";
+            } else if(document.getElementById("female").checked === true) {
+                cell[3].innerHTML = "여성";
+            }
+            cell[4].innerHTML = G_phone_number;
+            cell[5].innerHTML = G_join_date;
+            cell[6].innerHTML = G_join_date;
+            cell[7].innerHTML = G_endup_date;
+            cell[8].innerHTML = G_weight_at_join;
+            cell[9].innerHTML = G_weight_at_join;
+            cell[10].innerHTML = G_target_weight;
+            // 열 추가 종료
+        
+            member_index = member_index + 1;        //인덱스 값 증가
     }
-    
 }
-
-//더미데이터 출력용 함수
-function init(name, age, sex, phone_number, join_date, payment_date, endup_date, weight_at_join, weight_at_update, target_weight){
-        var table = document.getElementById("membertable");
-        var row = table.insertRow(-1);      //마지막 부분에 행 추가
-        var col = 0;
-        var cell = new Array()
-        while(col < 11) {
-            cell[col] = row.insertCell(col)
-            col = col + 1;
-        }
-
-        cell[0].innerHTML = member_index+` <input type="radio" name="slect" value="`+ member_index +`"></input>`;
-        cell[1].innerHTML = name;
-        cell[2].innerHTML = age;
-        cell[3].innerHTML = sex;
-        cell[4].innerHTML = phone_number;
-        cell[5].innerHTML = join_date;
-        cell[6].innerHTML = payment_date;
-        cell[7].innerHTML = endup_date;
-        cell[8].innerHTML = weight_at_join;
-        cell[9].innerHTML = weight_at_update;
-        cell[10].innerHTML = target_weight;
-
-        member_index = member_index + 1;
-}
-
-init("안지수", "27", "남", "315-4831-7921", "2020-02-10", "2020-05-10", "2020-08-10", "89", "87", "75");
-init("박남일", "22", "남", "315-4641-3133", "2020-06-10", "2020-06-10", "2020-09-10", "85", "85", "80");
