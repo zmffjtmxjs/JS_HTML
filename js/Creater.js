05258b9d6916ed56a4fbce5ef37644218a08d9fe
Clustern <homerun372@gmail.com>
2020-06-11 22:02:14 +0900

    리스트 내 라디오 버튼에 대한 반응 추가[불안정]

--- js/Creater.js
+++ js/Creater.js
@@ -1,18 +1,33 @@
 //삽입 기능관련 코드만 포함
 
 //더미데이터 출력용 함수, member_add()함수의 뿌리. [유효성 검사 생략함]
-function dumiDataPutter(name, age, sex, phone_number, join_date, payment_date, endup_date, weight_at_join, weight_at_update, target_weight){
-    var table = document.getElementById("membertable");
-    table.deleteRow(-1);
+function dumiDataPutter(name, age, sex, phone_number, join_date, payment_date, endup_date, weight_at_join, weight_at_update, target_weight, Q_delrow){
+    var table = document.getElementById("membertable"); //수정 목표 테이블 지정
+    var CTID = `CTID_`+member_index;       //id 지정에 쓰일 변수
+
+    if(Q_delrow){
+        table.deleteRow(-1);                   //신규 버튼을 위한 더미 행 삭제
+    }
+    
     var insrow = table.insertRow(-1);      //마지막 부분에 행 추가
-    var col = 0;
+
+    var col = 0;                           //11개의 열에 대한 배열
     var cell = new Array()
     while(col < 11) {
         cell[col] = insrow.insertCell(col)
         col = col + 1;
     }
-    insrow.setAttribute("id", `CTID_`+member_index);
-    cell[0].innerHTML = member_index+` <input type="radio" name="CT_select" value="CTID_`+member_index +`"></input>`;
+
+    insrow.setAttribute("id", CTID);       //추가될 행에 대한 id값 추가
+
+    col = 0;                               //11개의 열에 대한 id값 추가
+    while(col < 11) {
+        cell[col].setAttribute("id", CTID+`-`+col)
+        col = col + 1;
+    }
+
+
+    cell[0].innerHTML = member_index+` <input type="radio" name="CT_select" value="CTID_`+member_index +`" onclick="G_listRadio_action()"></input>`;
     cell[1].innerHTML = name;
     cell[2].innerHTML = age;
     cell[3].innerHTML = sex;
@@ -35,103 +50,140 @@ function dumiDataPutter(name, age, sex, phone_number, join_date, payment_date, e
     }
     col = 1;
     insrow.setAttribute("id", "N");
-    cell[0].innerHTML = `N <input type="radio" name="CT_select" value="new"></input>`;
+    cell[0].innerHTML = `N <input type="radio" name="CT_select" value="new" onclick="G_listRadio_action()"></input>`;
     while(col < 11) {
         cell[col].innerHTML = "-";
         col = col + 1;
     }
 }
 
-dumiDataPutter("안지수", "27", "남", "010-4831-7921", "2020-02-10", "2020-05-10", "2020-08-10", "89", "87", "75");
-dumiDataPutter("박남일", "22", "남", "010-4641-3133", "2020-06-10", "2020-06-10", "2020-09-10", "85", "85", "80");
+dumiDataPutter("안지수", "27", "남", "010-4831-7921", "2020-02-10", "2020-05-10", "2020-08-10", "89", "87", "75", 0);
+dumiDataPutter("박남일", "22", "남", "010-4641-3133", "2020-06-10", "2020-06-10", "2020-09-10", "85", "85", "80", 1);
 
-//종합적인 입력값 유효성 검사 (삽입 기능 전용)
-function G_data_input_checker(){
-    //G_data_checker_logger() //출력값 테스트 개발자용
-    if(G_name === ''){
-        alert("이름을 입력해주세요.");
-        return false;
-    } else if((G_age == '') || (Number.isInteger(G_age))){
-        alert("입력한 나이를 확인해주세요.");
-        return false;
-    } else if((G_phone_number == '') || (isNotCellPhone(G_phone_number))){
-        alert("입력한 전화번호를 확인해주세요.");
-        return false;
-    } else if((G_join_date == '') || (isNotDatetime(G_join_date))){
-        alert("입력한 가입일을 확인해주세요.");
-        return false;
-    } else if((G_endup_date == '') || (isNotDatetime(G_endup_date))){
-        alert("입력한 만료일을 확인해주세요.");
-        return false;
-    } else if((G_weight_at_join == '') || (Number.isInteger(G_weight_at_join))){
-        alert("입력한 현재 몸무게를 확인해주세요.");
-        return false;
-    } else if((G_target_weight == '') || (Number.isInteger(G_target_weight))){
-        alert("입력한 목표 몸무게를 확인해주세요.");
-        return false;
-    } else if((document.getElementById("male").checked === false)&&(document.getElementById("female").checked === false)){
-        alert("성별을 체크해주세요.");
-        return false;
-    } else{
-        return true;
+//입력값 유효성 검사 (삽입 기능 전용)
+function G_value_checker(G_values){
+    for (var arr_num = 0; arr_num <= 9; arr_num++){
+        var V_check = G_values[arr_num];
+        console.log(V_check);
     }
+    console.log("배열 출력 끝");
+    for (var arr_num = 0; arr_num <= 9; arr_num++){
+        var V_check = G_values[arr_num];
+        console.log(V_check);
+        switch(arr_num){
+            case 0:
+                if(V_check === ''){
+                    alert("이름을 입력해주세요.");
+                    return false;
+                }
+                break;
+            case 1:
+                if((V_check == '') || (Number.isInteger(V_check))){
+                    alert("입력한 나이를 확인해주세요.");
+                    return false;
+                }
+                break;
+            case 2:
+                if(G_name === ''){
+                    alert("성별을 체크해주세요.");
+                    return false;
+                }
+                break;
+            case 3:
+                if((V_check == '') || (isNotCellPhone(V_check))){
+                    alert("입력한 전화번호를 확인해주세요.");
+                    return false;
+                }
+                break;
+            case 4:
+                if((V_check == '') || (isNotDatetime(V_check))){
+                    alert("입력한 가입일을 확인해주세요.");
+                    return false;
+                }
+                break;
+            case 6:
+                if((V_check == '') || (isNotDatetime(V_check))){
+                    alert("입력한 만료일을 확인해주세요.");
+                    return false;
+                }
+                break;
+            case 7:
+                if((V_check == '') || (Number.isInteger(V_check))){
+                    alert("입력한 현재 몸무게를 확인해주세요.");
+                    return false;
+                }
+                break;
+            case 9:
+                if((V_check == '') || (Number.isInteger(V_check))){
+                    alert("입력한 목표 몸무게를 확인해주세요.");
+                    return false;
+                }
+                break;
+        }
+    }
+    return true;
 }
 
-function member_add(){
-    G_name = document.getElementById("G_name").value;
-    G_age = document.getElementById("G_age").value;
-    G_phone_number = document.getElementById("G_phone_number").value;
-    G_join_date = document.getElementById("G_join_date").value;
-    G_endup_date = document.getElementById("G_endup_date").value;
-    G_weight_at_join = document.getElementById("G_weight_at_join").value;
-    G_target_weight = document.getElementById("G_target_weight").value;
-    var checker = G_data_input_checker();
+//신규 멤버 추가 함수
+function member_add(){                                                  
+    var G_values = G_value_getter();                                    //입력된 값들을 변수로 가져오기
+    var checker = G_value_checker(G_values);                            //유효성 검사기
+    var col;                                                            //반복문 '행' 카운터
 
     if(checker){
-        //테이블 행 추가를 위한 변수
-        var table = document.getElementById("membertable");
-        table.deleteRow(-1);
-        var insrow = table.insertRow(-1);      //마지막 부분에 행 추가
-        //열 추가
-        var col = 0;                        
-        var cell = new Array()
-        while(col < 11) {
-        cell[col] = insrow.insertCell(col)
-            col = col + 1;
-        }
-        //<tr>태그에 id값 추가
-        insrow.setAttribute("id", `CTID_`+member_index);
-        // 열 내용 추가
-        cell[0].innerHTML = member_index+` <input type="radio" name="CT_select" value="CTID_`+member_index +`"></input>`;
-        cell[1].innerHTML = G_name;
-        cell[2].innerHTML = G_age;
+        var G_name = document.getElementById("G_name").value;
+        var G_age = document.getElementById("G_age").value;
         if(document.getElementById("male").checked === true) {
-            cell[3].innerHTML = "남성";
+            var G_sex = "남성";
         } else if(document.getElementById("female").checked === true) {
-            cell[3].innerHTML = "여성";
+            var G_sex = "여성";
         }
-        cell[4].innerHTML = G_phone_number;
-        cell[5].innerHTML = G_join_date;
-        cell[6].innerHTML = G_join_date;
-        cell[7].innerHTML = G_endup_date;
-        cell[8].innerHTML = G_weight_at_join;
-        cell[9].innerHTML = G_weight_at_join;
-        cell[10].innerHTML = G_target_weight;
+        var G_phone_number = document.getElementById("G_phone_number").value;
+        var G_join_date = document.getElementById("G_join_date").value;
+        var G_payment_date = G_join_date;
+        var G_endup_date = document.getElementById("G_endup_date").value;
+        var G_weight_at_join = document.getElementById("G_weight_at_join").value;
+        var G_weight_at_update = G_weight_at_join;
+        var G_target_weight = document.getElementById("G_target_weight").value;
+        
+        var G_values = [G_name, G_age, G_sex, G_phone_number, G_join_date, G_payment_date, G_endup_date, G_weight_at_join, G_weight_at_update, G_target_weight]
+
+        var table = document.getElementById("membertable"); //수정 목표 테이블 지정
+        var CTID = `CTID_`+member_index;                //id 지정에 쓰일 변수 출력예시 : CTID_1
+    
+        table.deleteRow(-1);                            //신규 버튼을 위한 더미 행 삭제
+        
+        var insrow = table.insertRow(-1);               //마지막 부분에 행 추가
+
+        // 열 추가 작업
+        var cell = new Array()                          //11개의 열에 대한 배열
+        for(col=0; col < 11; col++) {
+            cell[col] = insrow.insertCell(col)
+        }
+    
+        insrow.setAttribute("id", CTID);                //추가될 행(<tr>)에 대한 id값 추가 [출력예시 : id="CTID_1"]
+        
+        for(col = 0 ;col < 11; col++) {                 //11개의 열(<td>)에 대한 id값 추가
+            cell[col].setAttribute("id", CTID+`-`+col)  //출력예시: CTID_1-1
+        }
+
+        cell[0].innerHTML = member_index+` <input type="radio" name="CT_select" value="CTID_`+member_index+` onclick="G_listRadio_action()""></input>`;
+        for(col = 1; col < 11; col++) {                 //내용 추가 작업
+            cell[col].innerHTML = G_values[(col-1)]
+        }
         // 열 추가 종료
+        document.getElementsByName('CT_select')
         
         member_index = member_index + 1;        //인덱스 값 증가
 
-        col = 0;
-        var insrow = table.insertRow(-1);      //신규 버튼을 위한 빈행 추가
-        while(col < 11) {
+        var insrow = table.insertRow(-1);       //신규 버튼을 위한 빈행 추가
+        for(col = 0; col < 11; col++) {
         cell[col] = insrow.insertCell(col)
-            col = col + 1;
         }
         col = 1;
-        cell[0].innerHTML = `N <input type="radio" name="CT_select" value="new"></input>`;
-        while(col < 11) {
+        cell[0].innerHTML = `N <input type="radio" name="CT_select" value="new" onclick="G_listRadio_action()"></input>`;
+        for(col = 1; col < 11; col++) {
             cell[col].innerHTML = "-";
-            col = col + 1;
         }
     }
 }
--- js/f_hub.js
+++ js/f_hub.js
@@ -1,14 +1,19 @@
 //Creater.js, Deleter.js, Updater.js 모두가 사용하는 코드(변수선언, 함수) 등을 포함
 
-var G_name = document.getElementById("G_name").value;
-var G_age = document.getElementById("G_age").value;
-var G_phone_number = document.getElementById("G_phone_number").value;
-var G_join_date = document.getElementById("G_join_date").value;
-var G_payment_date = document.getElementById("G_payment_date").value;
-var G_endup_date = document.getElementById("G_endup_date").value;
-var G_weight_at_join = document.getElementById("G_weight_at_join").value;
-var G_weight_at_update = document.getElementById("G_weight_at_update").value;
-var G_target_weight = document.getElementById("G_target_weight").value;
+// var G_name = document.getElementById("G_name").value;
+// var G_age = document.getElementById("G_age").value;
+// var G_sex
+// var G_phone_number = document.getElementById("G_phone_number").value;
+// var G_join_date = document.getElementById("G_join_date").value;
+// var G_payment_date = document.getElementById("G_payment_date").value;
+// var G_endup_date = document.getElementById("G_endup_date").value;
+// var G_weight_at_join = document.getElementById("G_weight_at_join").value;
+// var G_weight_at_update = document.getElementById("G_weight_at_update").value;
+// var G_target_weight = document.getElementById("G_target_weight").value;
+
+/*
+CTID_*는 각 고객정보에 대한 식별값임
+*/
 
 var member_index = 1;
 
@@ -29,3 +34,55 @@ function isNotCellPhone(str) {
 	}
 	return false;
 };
+
+//CT_select이름을 가진 radio 버튼중 선택된 것의 CTID값을 가져옴
+function CT_selecter(){
+    var d = document.getElementsByName('CT_select');
+    for (var i = 0; i < d.length; i++) {
+        if (d[i].checked) {          
+            return d[i].value;
+        }
+    }
+}
+
+//테이블 값 가져오기
+function CT_info(){
+    var CTID_num = CT_selecter();
+    var info_data = new Array();
+    for(var col = 1; col <= 10; col++) {
+        info_data[col-1] = document.getElementById(CTID_num+`-`+col).innerHTML;
+    }
+    return info_data;
+}
+
+//테이블 내에 있는 라디오 버튼에 대한 입력폼의 반응
+function G_listRadio_action(){
+    var Q_selected = CT_selecter()
+    var field_name = ["G_name", "G_age", "G_sex", "G_phone_number", "G_join_date", "G_payment_date", "G_endup_date", "G_weight_at_join", "G_weight_at_update", "G_target_weight"];
+    
+    if (Q_selected == "new"){                                //신규 사용자에 체크될 경우 입력폼의 값을 변수로 가져옴.
+        for(var col = 0; col < field_name.length; col++){
+            if(col != 3){
+            document.getElementById(field_name[col]).value = "";
+            }
+        }
+    } else if(Q_selected != undefined){                      //라디오가 신규(왼쪽 끝 하단)에 체크되어 있지 않으면 체크된 라디오 행의 데이터를 입력폼으로 가져옴.
+        var info_data = CT_info();
+        // console.log(Array.isArray(field_name));
+        // console.log(Array.isArray(info_data));
+        // console.log("필드네임 배열리스트");
+        // for(var i = 0; i <= field_name.length; i++){
+        //     console.log(field_name[i]);
+        // }
+        // console.log("인포 데이타 배열리스트");
+        // for(var i = 0; i <= info_data.length; i++){
+        //     console.log(info_data[i]);
+        // }
+        for(var col = 0; col < field_name.length; col++){
+            console.log(info_data[col]);
+            if(col != 2){
+            document.getElementById(field_name[col]).value = info_data[col];
+            }
+        }
+    }
+}
